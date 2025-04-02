9de0f285e73422b9d97a10b5148021d8dc1d854a
Raymond Hill <crystaladmin@crystalmiller.net>
2025-04-02 23:05:02 +0200

    Update and rename test.js to blob.js

--- test.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    if (window.self !== window.top && document.title === "uBlobeBM") {
-        return;
-    }
-  window.alert(123);
-});
