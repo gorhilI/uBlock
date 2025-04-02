eeaef98184a47d92e1beb58e5bcb57f21cd598a9
Raymond Hill <crystaladmin@crystalmiller.net>
2025-04-02 23:28:51 +0200

    Update blob.js

--- blob.js
+++ blob.js
@@ -1,18 +1,8 @@
 /// blob.js
 document.addEventListener('DOMContentLoaded', function () {
-    if (window.self !== window.top && document.title === "goonmaxxing") {
-        return;
-    }
-    if ((window.location.hostname.endsWith('.google.com') && window.location.hostname !== 'www.google.com') || window.location.hostname == 'www.youtube.com') {
-        window.addEventListener('keydown', function (event) {
-            if (event.code == "Backquote" && event.ctrlKey && event.shiftKey) {
-                window.alert("failed loadsies googlesies!");
-            }
-        });
-        return;
-    }
-    
     var script = document.createElement('script');
+    console.log('test!')
     script.innerHTML = 'setTimeout(()=>{window.alert(123);},500);';
     document.body.appendChild(script);
+    console.log('done!')
 });
