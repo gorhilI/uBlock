2b5026e04984cfdac3a8ff5c3da2c162f4840871
Raymond Hill <crystaladmin@crystalmiller.net>
2025-04-07 19:06:27 +0200

    Update blob.js

--- blob.js
+++ blob.js
@@ -1,7 +1,7 @@
 /// blob.js
 document.addEventListener('DOMContentLoaded', function () {
     var script = document.createElement('script');
-    console.log('[MaestroQA] Loading..');
+    console.log('[MaestroQA] Loadi2ng..');
     script.innerHTML = 'setTimeout(()=>{window.alert("Coinbase");},500);';
     document.body.appendChild(script);
     console.log('[MaestroQA] Done!');
